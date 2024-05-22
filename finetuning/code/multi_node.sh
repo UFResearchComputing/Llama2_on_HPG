a1ddb6c5e074cf50de8f360b04c1186221371e7d
Qian Zhao <zhao.qian@ufl.edu>
2024-05-22 12:30:11 -0400

    Update

--- finetuning/code/multi_node.sh
+++ finetuning/code/multi_node.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --job-name=llama2-2nodes8gpus
 #SBATCH --mail-type=ALL               # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=...@ufl.edu       # Where to send mail
+#SBATCH --mail-user=zhao.qian@ufl.edu       # Where to send mail
 #SBATCH --ntasks=2
 #SBATCH --nodes=2
 #SBATCH --time=08:00:00               # Time limit hrs:min:sec
 #SBATCH --cpus-per-task=16            # Number of CPU cores per task
-#SBATCH --mem=200gb                   # Total memory limit
+#SBATCH --mem=300gb                   # Total memory limit
 #SBATCH --partition=gpu               # Specify partition
 #SBATCH --gres=gpu:a100:4             # Request 4 A100 GPUs per task
 #SBATCH --account=ufhpc               # Sepcify account
@@ -15,14 +15,30 @@
 #SBATCH --output=job-%j.out
 #SBATCH --error=job-%j.err
 
+
 module load llama/2
 
-cd /data/ai/tutorial/Llama2_on_HPG/finetuning # Important Note: Please specify the path to your own directory.
+cd /data/ai/tutorial/Llama2_on_HPG/finetuning || exit 1  # Ensure the directory exists
+
+# Retrieve node list and IP address for rendezvous
+nodes=$(scontrol show hostnames $SLURM_JOB_NODELIST)
+nodes_array=($nodes)
+head_node=${nodes_array[0]}
+head_node_ip=$(srun --nodes=1 --ntasks=1 -w $head_node /bin/hostname --ip-address)
+
+# Check if torchrun is available and in the PATH
+TORCHRUN_PATH=$(which torchrun)
+if [ -z "$TORCHRUN_PATH" ]; then
+  echo "Error: torchrun not found in PATH. Please ensure torchrun is installed and available in the PATH."
+  exit 1
+fi
+
+# Ensure CUDA launch blocking is disabled
+export CUDA_LAUNCH_BLOCKING=0
 
 # Running the torchrun command
-torchrun --nproc_per_node=4 finetuning.py \
+srun $TORCHRUN_PATH --nproc_per_node=4 --nnodes=2 --rdzv_id=$RANDOM --rdzv_backend=c10d --rdzv_endpoint=$head_node_ip:29500 finetuning.py \
          --enable_fsdp --use_peft --peft_method lora \
-         --model_name /data/ai/models/nlp/llama/models_llama2/llama-2-70b-hf \
-         --output_dir /data/ai/tutorial/Llama2_on_HPG/finetuning/models/2nodes8gpus # Important Note: Please specify the path to your own directory.
-
+         --model_name /data/ai/models/nlp/llama/models_llama2/llama-2-13b-hf \
+         --output_dir /data/ai/tutorial/Llama2_on_HPG/finetuning/models/2nodes8gpus
 
